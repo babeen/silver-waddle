305b97704a48e24da86e69c5e68b6e06f6be1814
b@bin_singh_nepali <babinnepalibtl@gmail.com>
2023-03-27 18:53:14 +0545

    function and class component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//Creacting component
+//Function component
+// function Welcome(props) {
+//   return (<h1>Welcome {props.name}</h1>)
+// }
+
+//Class component
+class Welcome extends React.Component {
+  render() {
+    return (<h1>Welcom {this.props.name}</h1>)
+  }
+}
+
+ReactDOM.render(
+  <>
+    <Welcome name="Yushika" />,
+    <Welcome name="Kripa" />,
+    <Welcome name="Manish" />,
+
+  </>,
+
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
